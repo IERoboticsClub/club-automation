d1fdca1471abb804da373683247c05fc727bbc03
velocitatem <daniel@alves.world>
2024-02-11 23:37:12 +0100

    improved whatsapp integration and waiting. Waiting issue included whatsapp loading old messages and selenium waiting for it to finish.

--- communications/whatsapp.py
+++ communications/whatsapp.py
@@ -4,42 +4,64 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import logging as log
+# set logs to write to automation.log
+log.basicConfig(filename="/tmp/automation.log", level=log.INFO)
 
 
 class Whatsapp:
     def __init__(self, user_directory_path=None, headless=False, driver_path=None):
+        log.info("Initializing Whatsapp Instance at {}".format(time.ctime()))
         if not user_directory_path:
             user_directory_path = os.path.join(os.getcwd(),"user_data")
+            log.warning("No user directory path provided. Using default path")
         self.user_directory_path = user_directory_path
         if not driver_path:
             print("Please provide a driver path")
+            log.error("Please provide a driver path")
             sys.exit(1)
         self.driver_path = driver_path
         self.headless = headless
         self.driver = self.spawn_whatsapp_instance()
 
 
+
+
     def spawn_whatsapp_instance(self):
         # we use a chrome driver with store session
-        driver = webdriver.Chrome()
         print("OPENING WHATSAPP")
         options = webdriver.ChromeOptions()
         # MODULARIZE THIS
         options.add_argument(r"user-data-dir={}".format(self.user_directory_path))
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
-        headless = False
+        headless = self.headless
+        log.info("Spawning Whatsapp Instance")
         # make
         if headless:
+            print("HEADLESS MODE")
+            log.info("Using HEADLESS mode for whatsapp instance")
             options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
         # MODULARIZE THIS
         drier_path = self.driver_path
         os.environ["webdriver.chrome.driver"] = drier_path
         driver = webdriver.Chrome(options=options)
-        print("OPENED WHATSAPP")
         driver.get("https://web.whatsapp.com/")
-        driver.implicitly_wait(60) # it really takes a while
+        # wait for Loading your chats not not be in the page
+        print("OPENED WHATSAPP")
+        WebDriverWait(driver, 60).until(
+            # the search bar is visiblesWebDriverWait(driver, 60).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@contenteditable='true']"))
+
+
+        )
+
         print("LOGGED IN")
+        log.info("Logged into whatsapp")
         return driver
 
 
@@ -57,6 +79,7 @@ class Whatsapp:
         for contact in contacts:
             # print name
             print(f"SENDING MESSAGE TO {contact}")
+            log.info(f"Sending message to {contact}")
             # search for contact in search box
             try:
                 search_box = driver.find_elements(By.XPATH,"//div[@contenteditable='true']")[0]
@@ -78,9 +101,10 @@ class Whatsapp:
             except Exception as e:
                 print(e)
                 print("Something went wrong. Try again.")
+                log.error("Failed to broadcast message")
                 continue
 
-        time.sleep(2)
+        time.sleep(5)
         return driver
 
 
