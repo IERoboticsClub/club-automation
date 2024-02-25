d5146b08d290cc1ee9b5d6f63c65f93401481343
velocitatem <daniel@alves.world>
2024-02-26 00:23:31 +0100

    sematics updated

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+# install cron on the server
+apt-get install cron
+# start cron service
+service cron start
+sudo apt-get install xvfb
+sudo apt-get install chromium-chromedriver
+sudo apt-get install chromium-browser
 crontab ./cron.txt
